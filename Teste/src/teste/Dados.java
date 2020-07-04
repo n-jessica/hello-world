09e982a14a4b0064e716d37b8e7d52b76c1a7e36
n-jessica <n.jessica0411@gmail.com>
2020-07-04 11:47:26 -0300

    Teste commit JAVA

---
+++ Teste/src/teste/Dados.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package teste;
+
+/**
+ *
+ * @author jessica
+ */
+public class Dados {
+
+    private String nome;
+    private String cpf;
+    private String email;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Dados{" + "nome=" + nome + ", cpf=" + cpf + ", email=" + email + '}';
+    }
+
+}
---
+++ Teste/src/teste/Teste.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package teste;
+
+/**
+ *
+ * @author jessica
+ */
+public class Teste {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Dados d = new Dados();
+
+        d.setNome("Maria");
+        d.setCpf("678564323-45");
+        d.setEmail("maria@teste.com");
+
+        System.out.println("Dados: " + d.toString());
+    }
+
+}
